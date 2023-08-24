8ac5749eb88ed7a4a05f8d7ff3f80e0dd84cde8c
ll-SilentHill-ll <krasuny4@ukr.net>
2023-08-24 18:36:40 +0300

    Update

--- js/scrips.js
+++ js/scrips.js
@@ -1,10 +1,11 @@
 $(document).ready(function(){
   $('.heder-line').click(function(){
     $('.adapive_menu').toggleClass('show_menu')
-    $('body').addClass('nooverflow')
+    $('body').toggleClass('nooverflow')
   })
 
   $('.adaptive__list').click(function(){
     $('.adapive_menu').removeClass('show_menu')
+    $('body').removeClass('nooverflow')
   })
 })
